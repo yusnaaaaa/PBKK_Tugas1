7c7a1a6607280d054e0b36606636ff76062aa383
yusnaaaaa <91377793+yusnaaaaa@users.noreply.github.com>
2023-09-01 14:39:31 +0700

    Add files via upload

---
+++ CalculatorApp/CalculatorApp/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int nilai1 = int.Parse(textBox1.Text);
+            int nilai2 = int.Parse(textBox2.Text);
+            int hasil;
+            hasil = nilai1 + nilai2;
+            textBox3.Text = hasil.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int nilai1 = int.Parse(textBox1.Text);
+            int nilai2 = int.Parse(textBox2.Text);
+            int hasil;
+            hasil = nilai1 - nilai2;
+            textBox3.Text = hasil.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int nilai1 = int.Parse(textBox1.Text);
+            int nilai2 = int.Parse(textBox2.Text);
+            int hasil;
+            hasil = nilai1 * nilai2;
+            textBox3.Text = hasil.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int nilai1 = int.Parse(textBox1.Text);
+            int nilai2 = int.Parse(textBox2.Text);
+            int hasil;
+            hasil = nilai1 / nilai2;
+            textBox3.Text = hasil.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
